f33049ce6c53057a1b12ae1ba29657a4d11b4bcb
artadsm <arturanacleto2@gmail.com>
2022-10-23 21:03:40 -0300

    Commit2

--- src/template/Home/index.tsx
+++ src/template/Home/index.tsx
@@ -1,13 +1,113 @@
-import Test from 'components/TestComponent';
+/* eslint-disable jsx-a11y/alt-text */
+import NavComponent from 'components/NavComponent';
+import TrendingComponent from 'components/TrendingComponent';
+import { useState } from 'react';
 import * as S from './styles';
 
-const HomeTemplate = () => (
-    <S.Container>
-        <S.Title>
-            Esse template foi criado pela equipe de Research 21.1 para projetos
-            internos da Polijunior.
-        </S.Title>
-        <Test title="Esse componente é um teste" />
-    </S.Container>
-);
+const HomeTemplate = () => {
+    const [name, setName] = useState('Página Inicial');
+    // console.log(name);
+
+    return (
+        <>
+            <S.ScreenDiv>
+                <S.SideBardiv>
+                    <S.SideBarUpperDiv>
+                        <S.SideBarIconDiv>
+                            <S.SideBarIcon src="public/assets/Logo.svg" />
+                            <S.SideBarIconTitle>PiuPiwer</S.SideBarIconTitle>
+                        </S.SideBarIconDiv>
+                        <S.SideBarOptionsDiv>
+                            <NavComponent
+                                link="/"
+                                title="Página Inicial"
+                                img="src\public\assets\Home.png"
+                                setName={setName}
+                                selected={name === 'Página Inicial'}
+                            />
+                            <NavComponent
+                                link="/404"
+                                title="Notificações"
+                                img="/../public/assets/ic_outline-notifications.svg"
+                                setName={setName}
+                                selected={name === 'Notificações'}
+                            />
+                            <NavComponent
+                                link="/404"
+                                title="Mensagens"
+                                img="/../public/assets/jam_messages-alt.svg"
+                                setName={setName}
+                                selected={name === 'Mensagens'}
+                            />
+                            <NavComponent
+                                link="/404"
+                                title="Salvos"
+                                img="../assets/Logo.svg"
+                                setName={setName}
+                                selected={name === 'Salvos'}
+                            />
+                            <NavComponent
+                                link="/404"
+                                title="Perfil"
+                                img="/assets/Logo.svg"
+                                setName={setName}
+                                selected={name === 'Perfil'}
+                            />
+                            <NavComponent
+                                link="/404"
+                                title="Configurações"
+                                img="/assets/Logo.svg"
+                                setName={setName}
+                                selected={name === 'Configurações'}
+                            />
+                        </S.SideBarOptionsDiv>
+                    </S.SideBarUpperDiv>
+                    <S.ProfileDiv>
+                        <S.ProfileImg src="\public\assets\quadrados 1.png" />
+                        <S.ProfileName>Artur Anacleto</S.ProfileName>
+                    </S.ProfileDiv>
+                </S.SideBardiv>
+                <S.FeedDiv>
+                    <S.TopFeedDiv>
+                        <S.SearchBarDiv />
+                        <S.FeedInputDiv />
+                    </S.TopFeedDiv>
+                    <S.BottomFeedDiv />
+                </S.FeedDiv>
+                <S.TrendingDiv>
+                    <S.TrendingTopDiv>
+                        <S.TrendTitleText>Pius em Alta</S.TrendTitleText>
+                    </S.TrendingTopDiv>
+                    <S.TrendingBottomDiv>
+                        <TrendingComponent
+                            img=""
+                            title="Eleições 2022"
+                            content="Eleição em São Paulo: veja Tweets sobre a disputa de 2º turno entre Tarcísio e Haddad"
+                        />
+                        <TrendingComponent
+                            img=""
+                            title="#AppleTeacher"
+                            content="Na semana do Dia do Professor, agradecemos sua paixão e dedicação! "
+                        />
+                        <TrendingComponent
+                            img=""
+                            title="A Fazenda"
+                            content="'A Fazenda 14': Veja os Pius sobre o programa 🤠"
+                        />
+                        <TrendingComponent
+                            img="../../public/assets/volei.png"
+                            title="Vôlei"
+                            content="Sérvia derrota o Brasil e é campeã mundial de vôlei feminino"
+                        />
+                        <TrendingComponent
+                            img=""
+                            title="Clima tenso no NTec"
+                            content="Gomes é apelidado de Campos e se  irrita com companheiros"
+                        />
+                    </S.TrendingBottomDiv>
+                </S.TrendingDiv>
+            </S.ScreenDiv>
+        </>
+    );
+};
 export default HomeTemplate;
--- src/template/Home/styles.ts
+++ src/template/Home/styles.ts
@@ -1,14 +1,167 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-    font-size: 2rem;
-    color: black;
+export const ScreenDiv = styled.div`
+    display: flex;
+    min-height: 100vh;
+    width: 100vw;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #3e4347;
+`;
+export const SideBardiv = styled.div`
+    display: flex;
+    height: 100vh;
+    width: 16%;
+    flex-direction: column;
+    justify-content: space-between;
+    padding-top: 32px;
+    padding-bottom: 32px;
+`;
+export const SideBarUpperDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: 416;
+    gap: 24px;
 `;
 
-export const Container = styled.div`
+export const SideBarIconDiv = styled.div`
     display: flex;
+    width: 100%;
+    height: 56px;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+`;
+export const SideBarIcon = styled.img`
+    width: 56px;
+    height: 56px;
+`;
+export const SideBarIconTitle = styled.span`
+    font-size: 36px;
+    font-family: 'Nunito';
+    color: #7b93ff;
+`;
+export const SideBarOptionsDiv = styled.div`
+    display: flex;
+    width: 100%;
+    height: 336px;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    padding-right: 56px;
+`;
+
+export const ProfileDiv = styled.div`
+    display: flex;
+    height: 48px;
+    width: 100%;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+`;
+export const ProfileImg = styled.img`
+    width: 64px;
+    height: 64px;
+    border-radius: 64px;
+`;
+export const ProfileName = styled.span`
+    font-size: 16px;
+    color: #e1e7ff;
+    font-weight: bold;
+    font-family: 'Nunito';
+`;
+export const SideBarText = styled.span`
+    font-size: 18px;
+    color: #e1e7ff;
+    font-style: bold;
+    font-family: 'Nunito Sans', sans-serif;
+`;
+export const FeedDiv = styled.div`
+    display: flex;
+    height: 100vh;
+    width: 60%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const TopFeedDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    width: 100%;
+    height: 276px;
+    padding-right: 64px;
+    padding-left: 64px;
+    margin: 0px;
+`;
+export const SearchBarDiv = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 32px;
+    width: 1004px;
+    height: 64px;
+    background-color: #9198b4;
+    border-radius: 16px;
+    margin-top: 32px;
+    margin-bottom: 40px;
+`;
+export const SerachBar = styled.input`
+    width: 500px;
+    height: 64px;
+`;
+export const FeedInputDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 140px;
+    width: 1004px;
+    border-radius: 16px;
+    background-color: #9198b4;
+    padding-left: 40px;
+`;
+
+export const BottomFeedDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+    padding-top: 32px;
+    height: 100%;
+    width: 100%;
+    padding-left: 64px;
+    padding-right: 64px;
+`;
+export const TrendingDiv = styled.div`
+    display: flex;
+    width: 24%;
     height: 100vh;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    overflow-y: scroll;
+`;
+export const TrendingTopDiv = styled.div`
+    height: 118px;
+    width: 100%;
+    padding-top: 40px;
+    padding-bottom: 32px;
+    padding-left: 24px;
+    align-items: flex-start;
+    justify-content: flex-start;
+`;
+export const TrendTitleText = styled.h1`
+    font-size: 36;
+    font-family: 'Nunito';
+    font-weight: bold;
+    color: #e1e7ff;
+`;
+export const TrendingBottomDiv = styled.div`
+    height: 100%;
+    width: 100%;
 `;
